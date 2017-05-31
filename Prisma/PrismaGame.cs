8f03e5001303ff81bf5ee8f0c68a0cfac825d410
Pietro Carrara <piticarrara@gmail.com>
2017-05-31 00:37:31 +0000

    Initial commit

---
+++ Prisma/PrismaGame.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prisma
+{
+    public class PrismaGame : Game
+    {
+
+    }
+}
