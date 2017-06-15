dbdbb68e7f5cf01d7dfba06bb0121a8c9793a55e
Pietro Carrara <piticarrara@gmail.com>
2017-06-15 00:27:32 +0000

    Added method for measuring distances

--- Prisma/Math/Vector2Extensions.cs
+++ Prisma/Math/Vector2Extensions.cs
@@ -13,5 +13,12 @@ namespace Prisma
         {
             return FloatMath.Atan2(point.Y - self.Y, point.X - self.X);
         }
+
+        public static float DistanceTo(this Vector2 self, Vector2 point)
+        {
+            var dist = point - self;
+            
+            return (float)(Math.Sqrt(Math.Pow(dist.X, 2) + Math.Pow(dist.Y, 2)));
+        }
     }
 }
