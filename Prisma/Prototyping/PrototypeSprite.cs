49a98c3d3a51531fe456cbc1e6760a3dd4f9871a
Pietro Carrara <piticarrara@gmail.com>
2017-07-10 20:49:59 -0300

    Removed useless using

--- Prisma/Prototyping/PrototypeSprite.cs
+++ Prisma/Prototyping/PrototypeSprite.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework.Graphics;
-using System.Management.Instrumentation;
 
 namespace Prisma.Prototyping
 {
