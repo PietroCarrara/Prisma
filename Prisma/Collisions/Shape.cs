5bd6a3d85828d14623770c53d440a3dc4e52a854
Pietro Carrara <piticarrara@gmail.com>
2017-07-15 18:49:13 -0300

    Added a better collision check for Shapes

--- Prisma/Collisions/Shape.cs
+++ Prisma/Collisions/Shape.cs
@@ -8,6 +8,17 @@ namespace Prisma
 
 		}
 
+		public bool CollidesWith(Shape s)
+		{
+			if (s is Circle)
+				return CollidesWith((Circle)s);
+
+			if (s is Rectangle)
+				return CollidesWith((Rectangle)s);
+
+			return CollidesWith((Entity)s);
+		}
+
 		public abstract bool CollidesWith(Entity e);
 
 		public abstract bool CollidesWith(Circle c);
